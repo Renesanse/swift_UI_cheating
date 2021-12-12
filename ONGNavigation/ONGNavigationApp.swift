b8efe3bda64bb57c5869965e4dd28c2014fa4dbd
Renesanse <dsrenesanse@gmail.com>
2021-12-13 00:30:40 +0300

    Initial Commit

---
+++ ONGNavigation/ONGNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ONGNavigationApp.swift
+//  ONGNavigation
+//
+//  Created by Daniel Surnin on 13.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ONGNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
